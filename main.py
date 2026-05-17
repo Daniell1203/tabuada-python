4e08af218e3fd7776f8b2aa5af3b97657fd7128c
Carlos Daniel <danieldpaula07@gmail.com>
2026-05-17 12:04:54 -0300

    adiciona operacoes matematicas na tabuada

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 n = float(input("Digite um numero: "))
 
-print("=== TABUADA ===")
+print("=== SUPER TABUADA ===")
+
+print("\n--- MULTIPLICACAO ---")
 
 for i in range(1,11):
     print(f"{n} x {i} = {n*i}")
+
+print("\n--- DIVISAO ---")
+
+for i in range(1,11):
+    print(f"{n} / {i} = {n/i}")
+
+print("\n--- SOMA ---")
+
+for i in range(1,11):
+    print(f"{n} + {i} = {n+i}")
+
+print("\n--- SUBTRACAO ---")
+
+for i in range(1,11):
+    print(f"{n} - {i} = {n-i}")
